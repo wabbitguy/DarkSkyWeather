780d78d95f6fcb5a3103dedb3c45cd31eb428da3
Mel <arduino_mel@wabbitwanch.net>
2019-03-17 19:34:13 -0700

    Add files via upload

--- User_Setup.h
+++ User_Setup.h
@@ -12,7 +12,7 @@
 #define MAX_HOURS 24   // Maximum "hourly" forecast period, can be up 1 to 48
 //                     // Hourly forecast not used by TFT_eSPI examples
 
-#define MAX_DAYS 8     // Maximum "daily" forecast periods can be 1 to 8 (Today + 7 days = 8 maximum)
+#define MAX_DAYS 5     // Maximum "daily" forecast periods can be 1 to 8 (Today + 7 days = 8 maximum)
                        // TFT_eSPI example requires this to be >= 5 (today + 4 forecast days)
 
 // #define MINIMISE_DATA_POINTS // option to minimise stored values for TFT_eSPI_Weather example
@@ -29,7 +29,7 @@
 //#define SHOW_HEADER // Debug only - for checking response header via serial message
 //#define SHOW_JSON   // Debug only - simple serial output formatting of whole JSON message
 //#define SHOW_CALLBACK // Debug only to show the decode tree
-
+//
 // ###############################################################################
 // DO NOT tinker below, this is configuration checking that helps stop crashes:
 // ###############################################################################
